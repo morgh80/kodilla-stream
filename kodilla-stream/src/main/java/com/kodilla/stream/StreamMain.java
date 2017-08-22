e6a318319b92f37b87e106cac3fcd4304d472f2e
aeronaut <aeronaut@MacBook-Pro-aeronaut.local>
2017-08-22 22:51:00 +0200

    Excersise 7.5 ready for code review

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,26 +1,20 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.forumuser.Forum;
-import com.kodilla.stream.forumuser.ForumUser;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-
 public class StreamMain {
     public static void main(String[] args) {
 
-        System.out.println("Excersise 7.3");
-
-        Forum forum = new Forum();
-        Map<Integer, ForumUser> forumUsersList = forum.getForumUsersList().stream()
-                .filter(forumUser -> forumUser.getUserSex() == 'M')
-                .filter(forumUser -> forumUser.getAge() > 20)
-                .filter(forumUser -> forumUser.getUserPosts() > 0)
-                .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
-
-        forumUsersList.entrySet().stream()
-                .map(entry -> entry.getKey() + ": " + entry.getValue())
-                .forEach(System.out::println);
+//        System.out.println("Excersise 7.3");
+//
+//        Forum forum = new Forum();
+//        Map<Integer, ForumUser> forumUsersList = forum.getForumUsersList().stream()
+//                .filter(forumUser -> forumUser.getUserSex() == 'M')
+//                .filter(forumUser -> forumUser.getAge() > 20)
+//                .filter(forumUser -> forumUser.getUserPosts() > 0)
+//                .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
+//
+//        forumUsersList.entrySet().stream()
+//                .map(entry -> entry.getKey() + ": " + entry.getValue())
+//                .forEach(System.out::println);
 
     }
 
